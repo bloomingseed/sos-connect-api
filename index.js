63734c4df1785af5db7967fbcb62c3312dd36ce4
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2021-11-30 22:08:41 +0700

    Added CORS for all routes, all methods

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
 const routers = require("./routers");
 
+app.use(cors());
 app.use(express.json());
 // console.log(routers);
 routers.forEach((router) => app.use(`/${router.name}`, router.router));
 
 app.listen(port, () => {
-  console.log(`Example app listening at port: ${port}`)
-})
+  console.log(`Example app listening at port: ${port}`);
+});
