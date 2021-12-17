e9b3bf3d412b8db709876cf31bc72d6b21177a59
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2021-12-18 02:22:02 +0700

    Added commenting feature for requests and supports

--- models/supports.js
+++ models/supports.js
@@ -1,38 +1,52 @@
 const createModel = (sequelize, DataTypes) => {
-  const Supports = sequelize.define('Supports', {
-    id_support: {
-      primaryKey: true,
-      autoIncrement: true,
-      type: DataTypes.INTEGER
+  const Supports = sequelize.define(
+    "Supports",
+    {
+      id_support: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+      id_request: {
+        type: DataTypes.INTEGER,
+      },
+      username: {
+        type: DataTypes.STRING,
+      },
+      content: {
+        type: DataTypes.TEXT,
+      },
+      is_confirmed: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      date_created: {
+        type: DataTypes.DATE,
+      },
+      is_deleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
     },
-    id_request: {
-      type: DataTypes.INTEGER
-    },
-    username: {
-      type: DataTypes.STRING
-    },
-    content: {
-      type: DataTypes.TEXT
-    },
-    is_confirmed: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    date_created: {
-      type: DataTypes.DATE
-    },
-    is_deleted: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
+    {
+      timestamps: true,
+      createdAt: "date_created",
+      updatedAt: false,
     }
-  }, {
-    timestamps:true,
-    createdAt: 'date_created',
-    updatedAt: false
-  })
+  );
   Supports.associate = function (models) {
-    Supports.belongsTo(models.Requests, { foreignKey: 'id_request', as: 'request' })
-  }
-  return Supports
-}
-module.exports = createModel
+    Supports.belongsTo(models.Requests, {
+      foreignKey: "id_request",
+      as: "request",
+    });
+    Supports.hasMany(models.Comments, {
+      foreignKey: "id_support",
+      scope: {
+        object_type: 1,
+      },
+      as: "comments",
+    });
+  };
+  return Supports;
+};
+module.exports = createModel;
